80499903fdbabaf501616345e45555eb23d913fb
David Larkin <davidlarkin96@gmail.com>
2020-01-22 14:11:18 -0500

    fixed hover issues so that it now scales again with the Fade animation added

--- src/components/weatherDisplay.js
+++ src/components/weatherDisplay.js
@@ -7,47 +7,47 @@ import Slide from 'react-reveal/Slide'
 
 const WeatherDisplay = props => {
     const date = new Date();
-    let timestamp = date.getTime()/1000 // today's date
+    let timestamp = date.getTime() / 1000 // today's date
 
     function convertUNIX(unixTime) {
-		return new Date(unixTime * 1000);
-	}
+        return new Date(unixTime * 1000);
+    }
 
-	function kelvinToCelsius(kelvinTemp) {
-		return Math.round(kelvinTemp - 273);
-	}
+    function kelvinToCelsius(kelvinTemp) {
+        return Math.round(kelvinTemp - 273);
+    }
 
-	function kelvinToFahrenheit(kelvinTemp) {
-		return Math.round((kelvinTemp - 273) * 9 / 5 + 32)
-	}
+    function kelvinToFahrenheit(kelvinTemp) {
+        return Math.round((kelvinTemp - 273) * 9 / 5 + 32)
+    }
 
-	function convertToDate(inputDate) {
-		var date = convertUNIX(inputDate)
-		var month = helpers.convertMonthToString(date.getUTCMonth())
-		var day = helpers.convertDateToString(date.getUTCDay())
-		var dateString = '' + day + ', ' + month + ' ' + date.getUTCDate();
+    function convertToDate(inputDate) {
+        var date = convertUNIX(inputDate)
+        var month = helpers.convertMonthToString(date.getUTCMonth())
+        var day = helpers.convertDateToString(date.getUTCDay())
+        var dateString = '' + day + ', ' + month + ' ' + date.getUTCDate();
 
-		return dateString;
-	}
+        return dateString;
+    }
 
-	function convertToTime(inputDate) {
-		var date = convertUNIX(inputDate)
-		return convertMilitaryTime(date.getUTCHours())
-	}
+    function convertToTime(inputDate) {
+        var date = convertUNIX(inputDate)
+        return convertMilitaryTime(date.getUTCHours())
+    }
 
-	function convertMilitaryTime(time) {
-		if (time == 0) return '12 am'
-		else if (time < 12) return time + ' am'
-		else if (time == 12) return '12 pm'
-		else if (time > 12) return time - 12 + ' pm'
-	}
+    function convertMilitaryTime(time) {
+        if (time == 0) return '12 am'
+        else if (time < 12) return time + ' am'
+        else if (time == 12) return '12 pm'
+        else if (time > 12) return time - 12 + ' pm'
+    }
 
-	function filterByTime(time) {
-		var date = convertUNIX(time)
-		return date.getUTCHours()
+    function filterByTime(time) {
+        var date = convertUNIX(time)
+        return date.getUTCHours()
 
     }
-    
+
     return (
         <div>
             {props.data &&
@@ -56,14 +56,15 @@ const WeatherDisplay = props => {
                 </Fade>
             }
             {props.todaysIcon &&
-                <div className={styles.wrapper}>
-                    <Fade>
-                        <div className={styles.todaySection}> 
-                            <div className={styles.iconArea}>   
-                                <img src={helpers.convertIdToSVG(props.todaysIcon)} 
+                <Fade>
+                    <div className={styles.wrapper}>
+
+                        <div className={styles.todaySection}>
+                            <div className={styles.iconArea}>
+                                <img src={helpers.convertIdToSVG(props.todaysIcon)}
                                     alt="Today's weather icon"
                                     height='100'
-                                    wdith='100'/>
+                                    wdith='100' />
                             </div>
                             <div className={styles.dayArea}>
                                 <p>{convertToDate(timestamp)}</p>
@@ -76,31 +77,33 @@ const WeatherDisplay = props => {
                                 <p className={styles.degrees}>{kelvinToCelsius(props.todaysTemp)}º C</p>
                             </div>
                         </div>
-                    </Fade>
-                </div>
+                    </div>
+                </Fade>
             }
 
-            <div className={styles.container}>
-                {/* Filter so that only 3pm on each day is shown (estimate the weather)*/}
-                {props.data && props.data
-                    .filter(e => filterByTime(e.dt) == 15)
-                    .map(e => {
-                        return (
-                            <Fade>
-                            <div className={styles.section}>
-                                <p className={styles.date}>{convertToDate(e.dt)}</p>
-                                <img src={helpers.convertIdToSVG(e.weather[0].icon)}
-                                    alt={e.weather[0].main}
-                                    height='100'
-                                    width='100' />
-                                <p>{_.capitalize(e.weather[0].description)}</p>
-                                <p className={styles.fahrenheit}>{kelvinToFahrenheit(e.main.temp)}º F</p>
-                                <p className={styles.degrees}>{kelvinToCelsius(e.main.temp)}º C</p>
-                            </div>
-                            </Fade>
-                        )
-                    })}
-            </div>
+            <Fade>
+                <div className={styles.container}>
+                    {/* Filter so that only 3pm on each day is shown (estimate the weather)*/}
+                    {props.data && props.data
+                        .filter(e => filterByTime(e.dt) == 15)
+                        .map(e => {
+                            return (
+
+                                <div className={styles.section}>
+                                    <p className={styles.date}>{convertToDate(e.dt)}</p>
+                                    <img src={helpers.convertIdToSVG(e.weather[0].icon)}
+                                        alt={e.weather[0].main}
+                                        height='100'
+                                        width='100' />
+                                    <p>{_.capitalize(e.weather[0].description)}</p>
+                                    <p className={styles.fahrenheit}>{kelvinToFahrenheit(e.main.temp)}º F</p>
+                                    <p className={styles.degrees}>{kelvinToCelsius(e.main.temp)}º C</p>
+                                </div>
+                            )
+                        })}
+                </div>
+            </Fade>
+
         </div>
     )
 }
